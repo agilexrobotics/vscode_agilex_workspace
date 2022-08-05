f4f8dcf04ab065ef4c1d34d09584313aff5ecfb0
Anthony <originsun@126.com>
2022-08-05 16:58:21 +0800

    add helper script

---
+++ switch_to_chinese_source.sh
@@ -0,0 +1,34 @@
+
+echo -e "\e[32mNow switch your package source to China mirror source\e[0m"
+sudo sed -i "s/ports.ubuntu.com/mirrors.ustc.edu.cn/g" /etc/apt/sources.list
+sudo sed -i "s/archive.ubuntu.com/mirrors.ustc.edu.cn/g" /etc/apt/sources.list
+sudo sed -i "s/packages.ros.org/repo.huaweicloud.com/g" /etc/apt/sources.list.d/ros2-latest.list
+sudo apt  update
+
+echo -e "\e[32mNow switch your pip package source to China mirror source\e[0m"
+pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+
+echo -e "\e[32mNow update rosdep database using rosdepc....\e[0m"
+sudo pip install rosdepc
+sudo rosdepc init
+rosdepc update
+
+echo -e "\e[32mNow update ros2.repos ....\e[0m"
+for file in "setup.sh" "src/ros2.repos"
+do
+        a=$(awk "/ghproxy.com/{print}" $file)
+        if [ -n "$a" ]
+        then
+                echo "$file already update."
+        else
+                sed -i "s#https://github.com#https://ghproxy.com/https://github.com#g" $file
+        fi
+done
+
+a=$(awk "/^rosdepc/{print}" setup.sh)
+if [ -n "$a" ]
+then
+        echo "rosdepc already update."
+else
+        sed -i "s/rosdep/rosdepc/g" setup.sh
+fi
