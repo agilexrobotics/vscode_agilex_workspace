bdf48b61ed9da8dce361aaee4ca260c944370dbd
Anthony <originsun@126.com>
2022-08-05 11:40:54 +0800

    add ydlidar-sdk install script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+
+# Install ydlidar SDK for ydlidar package
+git clone https://github.com/YDLIDAR/YDLidar-SDK.git
+mkdir -p YDLidar-SDK/build
+cd YDLidar-SDK/build &&\
+cmake ..&&\
+make &&\
+make install &&\
+cd .. &&\
+pip install . &&\
+cd .. && rm -r YDLidar-SDK 
+
 vcs import < src/ros2.repos src
 sudo apt-get update
 rosdep update
